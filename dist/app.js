346bb6ffa7627fc98fff3b1dcd98c5d9b45ebf41
Kyle Alwyn <kyle@lemonaid.com>
2017-02-01 09:54:16 -0800

    https redirect

--- dist/app.js
+++ dist/app.js
@@ -1,53 +1,59 @@
 'use strict';
 
-var basePath = 'templates';
-var templates = [{
-  filename: 'mapbox.html',
-  title: 'Mapbox',
-  tags: ['password']
-}, {
-  filename: 'wealthfront.html',
-  title: 'Wealthfront',
-  tags: ['thank you', 'update', 'holidays']
-}, {
-  filename: 'hired.html',
-  title: 'Hired',
-  tags: ['update', 'feature']
-}, {
-  filename: 'yahoo.html',
-  title: 'Yahoo',
-  tags: ['security', 'alert']
-}, {
-  filename: 'dollar-shave-club.html',
-  title: 'Dollar Shave Club',
-  tags: ['ecommerce', 'gift']
-}, {
-  filename: 'patagonia.html',
-  title: 'Patagonia',
-  tags: ['ecommerce', 'deals']
-}, {
-  filename: 'npm.html',
-  title: 'NPM',
-  tags: ['newsletter']
-}, {
-  filename: 'albert.html',
-  title: 'Albert',
-  tags: ['update', 'feature']
-}, {
-  filename: 'code-school.html',
-  title: 'Code School',
-  tags: ['achievement']
-}];
+(function () {
+  var basePath = 'templates';
+  var templates = [{
+    filename: 'mapbox.html',
+    title: 'Mapbox',
+    tags: ['password']
+  }, {
+    filename: 'wealthfront.html',
+    title: 'Wealthfront',
+    tags: ['thank you', 'update', 'holidays']
+  }, {
+    filename: 'hired.html',
+    title: 'Hired',
+    tags: ['update', 'feature']
+  }, {
+    filename: 'yahoo.html',
+    title: 'Yahoo',
+    tags: ['security', 'alert']
+  }, {
+    filename: 'dollar-shave-club.html',
+    title: 'Dollar Shave Club',
+    tags: ['ecommerce', 'gift']
+  }, {
+    filename: 'patagonia.html',
+    title: 'Patagonia',
+    tags: ['ecommerce', 'deals']
+  }, {
+    filename: 'npm.html',
+    title: 'NPM',
+    tags: ['newsletter']
+  }, {
+    filename: 'albert.html',
+    title: 'Albert',
+    tags: ['update', 'feature']
+  }, {
+    filename: 'code-school.html',
+    title: 'Code School',
+    tags: ['achievement']
+  }];
 
-var templatesContainer = document.getElementById('templates');
+  var render = function render() {
+    var templatesContainer = document.getElementById('templates');
 
-templates.forEach(function (template) {
-  var href = basePath + '/' + template.filename;
-  var link = document.createElement('div');
-  link.classList.add('col');
+    templates.forEach(function (template) {
+      var href = basePath + '/' + template.filename;
+      var link = document.createElement('div');
+      link.classList.add('col');
 
-  link.innerHTML = '\n    <div class="card">\n      <div class="card-body">\n        <div class="responsive-embed responsive-embed-4x3">\n          <a href="' + href + '" class="cover"></a>\n          <iframe src="' + href + '" scrolling="no"></iframe>\n        </div>\n      </div>\n      <div class="card-footer">\n        <a href="' + href + '">\n          <h2 class="card-title">' + template.title + '</h2>\n        </a>\n        <div class="card-actions">\n          <div>\n            ' + template.tags.map(function (t) {
-    return '<span class="tag tag-' + t + '">' + t + '</span>';
-  }).join(',').replace(/,/g, '') + '\n          </div>\n          <span class="flex"></span>\n          <a href="' + href + '" class="download-button" download></a>\n        </div>\n      </div>\n    </div>\n  ';
-  templatesContainer.appendChild(link);
-});
+      link.innerHTML = '\n        <div class="card">\n          <div class="card-body">\n            <div class="responsive-embed responsive-embed-4x3">\n              <a href="' + href + '" class="cover"></a>\n              <iframe src="' + href + '" scrolling="no"></iframe>\n            </div>\n          </div>\n          <div class="card-footer">\n            <a href="' + href + '">\n              <h2 class="card-title">' + template.title + '</h2>\n            </a>\n            <div class="card-actions">\n              <div>\n                ' + template.tags.map(function (t) {
+        return '<span class="tag tag-' + t + '">' + t + '</span>';
+      }).join(',').replace(/,/g, '') + '\n              </div>\n              <span class="flex"></span>\n              <a href="' + href + '" class="download-button" download></a>\n            </div>\n          </div>\n        </div>\n      ';
+      templatesContainer.appendChild(link);
+    });
+  };
+
+  render();
+})();
--- src/app.js
+++ src/app.js
@@ -1,70 +1,76 @@
-const basePath = 'templates'
-const templates = [{
-  filename: 'mapbox.html',
-  title: 'Mapbox',
-  tags: ['password']
-}, {
-  filename: 'wealthfront.html',
-  title: 'Wealthfront',
-  tags: ['thank you', 'update', 'holidays']
-}, {
-  filename: 'hired.html',
-  title: 'Hired',
-  tags: ['update', 'feature']
-}, {
-  filename: 'yahoo.html',
-  title: 'Yahoo',
-  tags: ['security', 'alert']
-}, {
-  filename: 'dollar-shave-club.html',
-  title: 'Dollar Shave Club',
-  tags: ['ecommerce', 'gift']
-}, {
-  filename: 'patagonia.html',
-  title: 'Patagonia',
-  tags: ['ecommerce', 'deals']
-}, {
-  filename: 'npm.html',
-  title: 'NPM',
-  tags: ['newsletter']
-}, {
-  filename: 'albert.html',
-  title: 'Albert',
-  tags: ['update', 'feature']
-}, {
-  filename: 'code-school.html',
-  title: 'Code School',
-  tags: ['achievement']
-}];
+(() => {
+  const basePath = 'templates'
+  const templates = [{
+    filename: 'mapbox.html',
+    title: 'Mapbox',
+    tags: ['password']
+  }, {
+    filename: 'wealthfront.html',
+    title: 'Wealthfront',
+    tags: ['thank you', 'update', 'holidays']
+  }, {
+    filename: 'hired.html',
+    title: 'Hired',
+    tags: ['update', 'feature']
+  }, {
+    filename: 'yahoo.html',
+    title: 'Yahoo',
+    tags: ['security', 'alert']
+  }, {
+    filename: 'dollar-shave-club.html',
+    title: 'Dollar Shave Club',
+    tags: ['ecommerce', 'gift']
+  }, {
+    filename: 'patagonia.html',
+    title: 'Patagonia',
+    tags: ['ecommerce', 'deals']
+  }, {
+    filename: 'npm.html',
+    title: 'NPM',
+    tags: ['newsletter']
+  }, {
+    filename: 'albert.html',
+    title: 'Albert',
+    tags: ['update', 'feature']
+  }, {
+    filename: 'code-school.html',
+    title: 'Code School',
+    tags: ['achievement']
+  }];
 
-const templatesContainer = document.getElementById('templates');
+  const render = () => {
+    const templatesContainer = document.getElementById('templates');
 
-templates.forEach(template => {
-  const href = `${basePath}/${template.filename}`;
-  const link = document.createElement('div');
-  link.classList.add('col');
+    templates.forEach(template => {
+      const href = `${basePath}/${template.filename}`;
+      const link = document.createElement('div');
+      link.classList.add('col');
 
-  link.innerHTML = `
-    <div class="card">
-      <div class="card-body">
-        <div class="responsive-embed responsive-embed-4x3">
-          <a href="${href}" class="cover"></a>
-          <iframe src="${href}" scrolling="no"></iframe>
-        </div>
-      </div>
-      <div class="card-footer">
-        <a href="${href}">
-          <h2 class="card-title">${template.title}</h2>
-        </a>
-        <div class="card-actions">
-          <div>
-            ${template.tags.map(t => `<span class="tag tag-${t}">${t}</span>`).join(',').replace(/,/g, '')}
+      link.innerHTML = `
+        <div class="card">
+          <div class="card-body">
+            <div class="responsive-embed responsive-embed-4x3">
+              <a href="${href}" class="cover"></a>
+              <iframe src="${href}" scrolling="no"></iframe>
+            </div>
           </div>
-          <span class="flex"></span>
-          <a href="${href}" class="download-button" download></a>
+          <div class="card-footer">
+            <a href="${href}">
+              <h2 class="card-title">${template.title}</h2>
+            </a>
+            <div class="card-actions">
+              <div>
+                ${template.tags.map(t => `<span class="tag tag-${t}">${t}</span>`).join(',').replace(/,/g, '')}
+              </div>
+              <span class="flex"></span>
+              <a href="${href}" class="download-button" download></a>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  `
-  templatesContainer.appendChild(link)
-});
+      `
+      templatesContainer.appendChild(link)
+    });
+  }
+
+  render();
+})();
